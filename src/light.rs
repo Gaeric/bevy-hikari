a4941ca8f660c1f3f1d109188a8a10acaab1209e
Gaeric <1062217098@qq.com>
2025-01-23 08:52:48 +0800

    format light

--- src/light.rs
+++ src/light.rs
@@ -44,7 +44,7 @@ impl Plugin for LightPlugin {
                     Render,
                     (
                         prepare_light_pass_targets.in_set(RenderSet::PrepareResources),
-                        (prepare_frame_uniform.in_set(RenderSet::Prepare)),
+                        prepare_frame_uniform.in_set(RenderSet::Prepare),
                         queue_view_bind_groups.in_set(RenderSet::Queue),
                         queue_light_bind_groups.in_set(RenderSet::Queue),
                         queue_light_pipelines.in_set(RenderSet::Queue),
